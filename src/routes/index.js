7128d85a4080645532940115edab173f92847563
amiryiz <amiryiz@gmail.com>
2018-08-01 14:26:24 +0300

    start-implementing material-UI

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Route, IndexRoute} from 'react-router'
 import Template from '../containers/Template'
+import Home from '../containers/Home'
+import Profile from '../containers/Profile'
 
 const createRoutes = () => { //declaring a function that is going to return our routes, the const is a variable that we'll be able to call later when we want to create these routes
     return (
@@ -8,6 +10,13 @@ const createRoutes = () => { //declaring a function that is going to return our 
         path='/' //root route
         component={Template} 
         >
+            <IndexRoute
+              component={Home}      
+            />
+            <Route
+              path={'/profile'}
+              component={Profile}
+            />
         </Route> 
     )
 }
