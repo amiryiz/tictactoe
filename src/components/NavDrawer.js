f5f542669a2493edcdf7abc04dd9e295430e9e89
amiryiz <amiryiz@gmail.com>
2018-08-06 12:12:10 +0300

    end - Nav drawer links

--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -4,23 +4,59 @@ import MenuItem from 'material-ui/MenuItem'
 import Divider from 'material-ui/Divider'
 import FloatingActionButton from 'material-ui/FloatingActionButton'
 import Menu from 'material-ui/svg-icons/navigation/menu'
-
+import {Link} from 'react-router'
 
 class NavDrawer extends Component {
+    state = {
+        open: true
+    }
+
+    toggle = () => {
+        this.setState((prevState, props) => {
+            return {
+                open: !prevState.open    
+            }    
+        })
+    }
+
     render() {
         return (
             <div>
-                <FloatingActionButton>
+                <FloatingActionButton
+                    onTouchTap={this.toggle}
+                >
                     <Menu/>
                 </FloatingActionButton>  
-                <Drawer>
+                <Drawer
+                    open={this.state.open}
+                >
+                    <div
+                        style={{
+                            height: '200px',
+                            width: '100%',
+                            backgroundColor:'green'
+                        }}
+                    >
+                        LoginContainer
+                    </div>
+
                     <Divider/>
-                    <MenuItem
-                        primaryText={'Play'}
-                    />
-                    <MenuItem
-                        primaryText={'Profile'}
-                    />
+                    <Link
+                        to={'/'}
+                    >
+                        <MenuItem
+                            onTouchTap={this.toggle}
+                            primaryText={'Play'}
+                        />
+                    </Link>
+                    <Link
+                        to={'/profile'}
+                    >
+                        <MenuItem
+                            onTouchTap={this.toggle}
+                            primaryText={'Profile'}
+                        />
+                    </Link>
                 </Drawer>  
             </div>
         )
